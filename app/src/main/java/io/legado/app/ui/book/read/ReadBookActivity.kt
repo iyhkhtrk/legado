0cda06c7caa3b70f329f864c4a38fbfe67809a68
iyhkhtrk <2752773186@qq.com>
2024-07-28 20:23:34 +0800

    ~

--- app/src/main/java/io/legado/app/ui/book/read/ReadBookActivity.kt
+++ app/src/main/java/io/legado/app/ui/book/read/ReadBookActivity.kt
@@ -109,7 +109,6 @@ import io.legado.app.utils.iconItemOnLongClick
 import io.legado.app.utils.invisible
 import io.legado.app.utils.isAbsUrl
 import io.legado.app.utils.isTrue
-import io.legado.app.utils.launch
 import io.legado.app.utils.navigationBarGravity
 import io.legado.app.utils.navigationBarHeight
 import io.legado.app.utils.observeEvent
@@ -291,12 +290,12 @@ class ReadBookActivity : BaseReadBookActivity(),
         }
     }
 
-    override fun onNewIntent(intent: Intent?) {
+    override fun onNewIntent(intent: Intent) {
         super.onNewIntent(intent)
         if (!ReadBook.inBookshelf) {
             viewModel.removeFromBookshelf(null)
         }
-        viewModel.initData(intent ?: return)
+        viewModel.initData(intent)
     }
 
     override fun onWindowFocusChanged(hasFocus: Boolean) {
@@ -590,9 +589,9 @@ class ReadBookActivity : BaseReadBookActivity(),
     /**
      * 按键拦截,显示菜单
      */
-    override fun dispatchKeyEvent(event: KeyEvent?): Boolean {
-        val keyCode = event?.keyCode
-        val action = event?.action
+    override fun dispatchKeyEvent(event: KeyEvent): Boolean {
+        val keyCode = event.keyCode
+        val action = event.action
         val isDown = action == 0
 
         if (keyCode == KeyEvent.KEYCODE_MENU) {
@@ -1320,7 +1319,7 @@ class ReadBookActivity : BaseReadBookActivity(),
                 }
 
                 "menu" -> showActionMenu()
-                "selectFolder" -> selectImageDir.launch()
+                "selectFolder" -> selectImageDir.launch(null)
             }
             popupAction.dismiss()
         }
