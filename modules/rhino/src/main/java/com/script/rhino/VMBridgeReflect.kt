1d3698d8a7cab48c1332528b629d1d9e29a0d95c
iyhkhtrk <2752773186@qq.com>
2026-06-17 22:58:02 +0800

    ~

--- modules/rhino/src/main/java/com/script/rhino/VMBridgeReflect.kt
+++ modules/rhino/src/main/java/com/script/rhino/VMBridgeReflect.kt
@@ -1,18 +1,12 @@
 package com.script.rhino
 
-import org.mozilla.javascript.VMBridge
+import org.mozilla.javascript.Context
 
 object VMBridgeReflect {
 
-    val instance: VMBridge by lazy {
-        VMBridge::class.java.getDeclaredField("instance").apply {
-            isAccessible = true
-        }.get(null) as VMBridge
-    }
-
     val contextLocal: ThreadLocal<Any> by lazy {
         @Suppress("UNCHECKED_CAST")
-        instance::class.java.getDeclaredField("contextLocal").apply {
+        Context::class.java.getDeclaredField("currentContext").apply {
             isAccessible = true
         }.get(null) as ThreadLocal<Any>
     }
