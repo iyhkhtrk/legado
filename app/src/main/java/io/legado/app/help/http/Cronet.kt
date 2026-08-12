21d4e6b0eb2f6ea549a291fb759185fd84e9a9f9
iyhkhtrk <2752773186@qq.com>
2026-08-13 05:23:25 +0800

    ~

--- app/src/main/java/io/legado/app/help/http/Cronet.kt
+++ app/src/main/java/io/legado/app/help/http/Cronet.kt
@@ -1,11 +1,11 @@
 package io.legado.app.help.http
 
 import io.legado.app.lib.cronet.CronetInterceptor
-import io.legado.app.lib.cronet.CronetLoader
+//import io.legado.app.lib.cronet.CronetLoader
 import okhttp3.Interceptor
 
 object Cronet {
-
+/*
     val loader: LoaderInterface? by lazy {
         CronetLoader
     }
@@ -13,11 +13,11 @@ object Cronet {
     fun preDownload() {
         loader?.preDownload()
     }
-
+*/
     val interceptor: Interceptor? by lazy {
         CronetInterceptor(cookieJar)
     }
-
+/*
     interface LoaderInterface {
 
         fun install(): Boolean
@@ -25,5 +25,5 @@ object Cronet {
         fun preDownload()
 
     }
-
+*/
 }
